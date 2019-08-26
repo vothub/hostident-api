ef85433c8b16cc646f347d2482cc01c4d4bad7c3
Vot <vzardzewialy@cambridge.org>
2019-08-26 01:52:40 +0100

    Add domain and URL lookups

--- routes/ui/actions/lookup-domain.get.js
+++ routes/ui/actions/lookup-domain.get.js
@@ -1,9 +1,13 @@
 const _ = require('lodash');
 
+const domainLib = require('../../../lib/domain');
+
 module.exports = (req, res) => {
   const lookupDomain = _.get(req, 'query.domain');
 
   console.log('lookupDomain', lookupDomain);
 
-  return res.render('lookup-results/domain', { query: { domain: lookupDomain }, result: {} });
+  return domainLib.lookup(lookupDomain, (domainErr, domainResult) => {
+    return res.render('lookup-results/domain', { query: { domain: lookupDomain }, result: domainResult });
+  });
 };
--- routes/ui/actions/lookup-url.get.js
+++ routes/ui/actions/lookup-url.get.js
@@ -1,9 +1,13 @@
 const _ = require('lodash');
 
+const urlLib = require('../../../lib/url');
+
 module.exports = (req, res) => {
   const lookupUrl = _.get(req, 'query.url');
 
   console.log('lookupUrl', lookupUrl);
 
-  return res.render('lookup-results/url', { query: { url: lookupUrl }, result: {} });
+  return urlLib.lookup(lookupUrl, (urlErr, urlResult) => {
+    return res.render('lookup-results/url', { query: { url: lookupUrl }, result: urlResult });
+  });
 };
